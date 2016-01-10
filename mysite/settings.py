1ee74d8311b036100aba436f14ca9b1c0a8ae3e6
Nash <hsuhang2001@gmail.com>
2016-01-10 03:10:06 -0500

    Config for heroku

--- mysite/settings.py
+++ mysite/settings.py
@@ -10,9 +10,6 @@ https://docs.djangoproject.com/en/1.7/ref/settings/
 
 LOGIN_REDIRECT_URL = "/index/"
 
-import os
-SECRET_KEY = os.environ['SECRET_KEY']
-
 import dj_database_url
 DATABASES['default'] = dj_database_url.config()
 
@@ -41,7 +38,9 @@ TEMPLATE_DIRS = (
 # See https://docs.djangoproject.com/en/1.7/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '$$0(71w-y1^p)0%#&()jy2-xh^q=^ny3=t(kac4oy2hduv!l5s'
+# SECRET_KEY = '$$0(71w-y1^p)0%#&()jy2-xh^q=^ny3=t(kac4oy2hduv!l5s'
+import os
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
