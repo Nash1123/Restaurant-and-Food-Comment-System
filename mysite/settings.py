5f7b4cf09cf926d0297445463d7eb630bf423e47
Nash <hsuhang2001@gmail.com>
2016-01-10 19:06:26 -0500

    Config for heroku

--- mysite/settings.py
+++ mysite/settings.py
@@ -78,15 +78,16 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 
 import dj_database_url
-DATABASES = {'default': dj_database_url.config()}
+# DATABASES = {'default': dj_database_url.config()}
 # DATABASES['default'] =  dj_database_url.config()
-"""
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+"""
 import dj_database_url
 DATABASES['default'] = dj_database_url.config()
 DATABASES = {'default': dj_database_url.config()}
